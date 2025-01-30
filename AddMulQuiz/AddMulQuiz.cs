53636606c2d8724002e876803e8dbd428ff47a47
sh.song <shsn013@gmail.com>
2025-01-30 17:47:00 +0900

    덧셈과 곱하기 문제 맞추는 게임 추가

---
+++ AddMulQuiz/AddMulQuiz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddMulQuiz
+{
+    internal class AddMulQuiz
+    {
+        public AddMulQuiz(bool add)
+        {
+            if (add) Op = "+";
+            else Op = "*";
+            N1 = R.Next(1, 30);
+            N2 = R.Next(1, 30);
+        }
+
+        public static Random R = new();
+        public int N1 { get; private set; }
+        public int N2 { get; private set; }
+        public string Op { get; private set; }
+
+        public bool Check(int a)
+        {
+            if (Op == "+") return (a == N1 + N2);
+            else return (a == N1 * N2);
+        }
+    }
+}
---
+++ AddMulQuiz/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AddMulQuiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddMulQuiz addMulQuiz = new(AddMulQuiz.R.Next(2) == 1);
+            while (true)
+            {
+                Console.Write($"{addMulQuiz.N1} {addMulQuiz.Op} {addMulQuiz.N2} = ");
+                if (!int.TryParse(Console.ReadLine(), out int i))
+                {
+                    Console.WriteLine("Thanks for playing!");
+                    return;
+                }
+                if (addMulQuiz.Check(i))
+                {
+                    Console.WriteLine("Right!");
+                    addMulQuiz = new AddMulQuiz(AddMulQuiz.R.Next(2) == 1);
+                }
+                else Console.WriteLine("Wrong! Try again.");
+            }
+        }
+    }
+}
