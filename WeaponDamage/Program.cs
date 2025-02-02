d4f37ec94613fd64a53fcf6d033b495ab204423d
sh.song <shsn013@gmail.com>
2025-02-02 23:48:06 +0900

    무기 데미지 계산기 추가

---
+++ WeaponDamage/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace WeaponDamage
+{
+    internal class Program
+    {
+        static Random random = new();
+        static void Main(string[] args)
+        {
+            SwordDamage swordDamage = new(RollDice(3));
+            ArrowDamage arrowDamage = new(RollDice(1));
+
+            while (true)
+            {
+                Console.Write("0 for no magic/flaming, 1 for magic, 2 for flaming, 3 for both, anything else to quit: ");
+                char key = Console.ReadKey().KeyChar;
+                if (key != '0' && key != '1' && key != '2' && key != '3') return;
+
+                Console.Write("\n S for word, A for arrow, anything else to quit: ");
+                char weaponKey = Char.ToUpper(Console.ReadKey().KeyChar);
+
+                switch (weaponKey)
+                {
+                    case 'S':
+                        swordDamage.Roll = RollDice(3);
+                        swordDamage.Magic = (key == '1' || key == '3');
+                        swordDamage.Flaming = (key == '2' || key == '3');
+                        Console.WriteLine($"\nRolled {swordDamage.Roll} for {swordDamage.Damage} HP\n");
+                        break;
+                    case 'A':
+                        arrowDamage.Roll = RollDice(3);
+                        arrowDamage.Magic = (key == '1' || key == '3');
+                        arrowDamage.Flaming = (key == '2' || key == '3');
+                        Console.WriteLine($"\nRolled {arrowDamage.Roll} for {arrowDamage.Damage} HP\n");
+                        break;
+                    default:
+                        return;
+                }
+            }
+        }
+
+        private static int RollDice(int numOfRolls)
+        {
+            int total = 0;
+            for (int i = 0; i < numOfRolls; i++) total += random.Next(1, 7);
+            return total;
+        }
+    }
+}
---
+++ WeaponDamage/SwordDamage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponDamage
+{
+    internal class SwordDamage(int startingRoll) : WeaponDamage(startingRoll)
+    {
+        private const int BASE_DAMAGE = 3;
+        private const int FLAME_DAMAGE = 2;
+
+        /// <summary>
+        /// 현재 속성들의 값을 기준으로 데미지를 계산합니다.
+        /// </summary>
+        protected override void CalculateDamage()
+        {
+            decimal magicMultiplier = 1M;
+            if (Magic) magicMultiplier = 1.75M;
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+            if (Flaming) Damage += FLAME_DAMAGE;
+        }
+    }
+}
